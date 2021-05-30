db34a0701d2b8951a12e3cff4100ac300a9b6a18
Nik Kelly <nikkelly@outlook.com>
2021-05-29 19:28:54 -0700

    added multiple settings

--- updatePimRoles.ps1
+++ updatePimRoles.ps1
@@ -1,35 +1,3 @@
-# Single setting update
-$tenantId = (Get-AzureadTenantDetail).ObjectId
-$roleSettings = Get-AzureADMSPrivilegedRoleSetting -ProviderId 'aadRoles' -Filter "ResourceId eq '$tenantId'"
-foreach ($roleSetting in $roleSettings){
-$setting = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedRuleSetting
-$setting.RuleIdentifier = "ApprovalRule"
-$setting.Setting= '{
-"Enabled": true,
-"IsCriteriaSupported": true,
-"Approvers": [
-  {
-    "Id": "e3bbd114-fbbb-4d53-b612-2f29520582ce",
-    "Type": "Group",
-    "DisplayName": "PIM Approvers",
-    "PrincipalName": ""
-  }
-],
-"HasNotificationPolicy": true
-}'
-Write-Host (Get-AzureADMSPrivilegedRoleDefinition -ProviderId 'aadRoles' -ResourceId $rolesetting.ResourceId -Id $rolesetting.RoleDefinitionId).DisplayName : $roleSetting.roleDefinitionId
-Set-AzureADMSPrivilegedRoleSetting -ProviderId 'aadRoles' -Id $roleSetting.Id -ResourceId $roleSetting.resourceId -RoleDefinitionId $roleSetting.roleDefeinintionId -UserMemberSettings $setting
-}
-
-## How do I skip the global admin account?
-foreach ($roleSetting in $roleSettings | Where-Object {$_.RoleDefinitionId -ne '62e90394-69f5-4237-9190-012177145e10'}){
-
-# Multiple Setting Update
-$tenantId = (Get-AzureadTenantDetail).ObjectId
-$roleSettings = Get-AzureADMSPrivilegedRoleSetting -ProviderId 'aadRoles' -Filter "ResourceId eq '$tenantId'"
-foreach ($roleSetting in $roleSettings | Where-Object {$_.RoleDefinitionId -ne '62e90394-69f5-4237-9190-012177145e10'}){
-$setting = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedRuleSetting
-#Settings go here
 # Set timeout to 4 hours
 $setting.RuleIdentifier = "ExpirationRule"
 $setting.Setting= '{
@@ -54,6 +22,35 @@ $setting3.RuleIdentifier = "ApprovalRule"
 $setting3.Setting= '{
 "Enabled": false
 }'
+
+
+$settingsTable = @{
+JustificationRule = '{
+  "required":true
+}'
+ApprovalRule = '{
+"Enabled": false
+}'
+MfaRule = '{
+  "mfaRequired":false
+  }'
+ExpirationRule = '{
+  "permanentAssignment":true,
+  "maximumGrantPeriodInMinutes":60
+  }'
+}
+
+# Multiple Setting Update
+$tenantId = (Get-AzureadTenantDetail).ObjectId
+$roleSettings = Get-AzureADMSPrivilegedRoleSetting -ProviderId 'aadRoles' -Filter "ResourceId eq '$tenantId'"
+foreach ($roleSetting in $roleSettings | Where-Object {$_.RoleDefinitionId -ne '62e90394-69f5-4237-9190-012177145e10'}){
+  $UserMemberSettings = @()
+  $settingstable.GetEnumerator() | Foreach-Object{
+    $setting = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedRuleSetting
+    $setting.RuleIdentifier = $_.key
+    $setting.Setting = $_.value
+    $UserMemberSettings += $setting
+  }
 Write-Host (Get-AzureADMSPrivilegedRoleDefinition -ProviderId 'aadRoles' -ResourceId $rolesetting.ResourceId -Id $rolesetting.RoleDefinitionId).DisplayName : $roleSetting.roleDefinitionId
-Set-AzureADMSPrivilegedRoleSetting -ProviderId 'aadRoles' -Id $roleSetting.Id -ResourceId $roleSetting.resourceId -RoleDefinitionId $roleSetting.roleDefeinintionId -UserMemberSettings $setting,$setting1,$setting2,$setting3
+Set-AzureADMSPrivilegedRoleSetting -ProviderId 'aadRoles' -Id $roleSetting.Id -ResourceId $roleSetting.resourceId -RoleDefinitionId $roleSetting.roleDefeinintionId -UserMemberSettings $UserMemberSettings
 }
